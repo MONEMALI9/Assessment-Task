c2b4f57f047a985a48ab92fcaa643a57935f6cbf
MONEMALI9 <monem921999@gmail.com>
2023-10-25 23:26:03 +0300

    .

--- task3/task.sh
+++ task3/task.sh
@@ -2,6 +2,9 @@
 
 mkdir -p company_info
 
+echo -e "Name,Age,PhoneNumber,Email,Salary,Department,CountryCode\nAllen,22,433 447-7961,Allen@vodafone.com.eg,800,IT,04\nWard,25,724-995-7762,Ward_ward@vodafone.com.eg,1600,Technology,22\nJones,24,987-123-4563,Jones*1@vodafone.com.eg,1250,Technology,04\nBlake,30,433-447-7964,Blake+30@vodafone.com.eg,2975,IT,04\nClark,28,1234567895,Clark.123@vodafone.com.eg,2850,IT,04\nScott,28,123-456-7896,Scott@vodafone-2.com.eg,2450,Technology,22\nKing,26,724-995-7767,King@vodafone.com.eg,3000,Technology,02\nTurner,35,433-447-7968,35Turner@vodafone.com.eg,5000,IT,02\nAdams,24,987 123 4569,Adams@vodafone.com.en.eg,1500,Technology,05 \nJames,22,123-456 (7810),James@vodafone.com.eg,1100,Technology,04\nMiller,23,142-156-7820,Miller-142@vodafone.com.eg,950,Technology,11\nFord,24,(724)-995-7339,Ford@vodafone.com.eg,1300,IT,22\nMartin,31,387-123-2561,Martin@vodafone.com..eg,3000,IT,02\nSmith,33,756-295-7722,Smith33@vodafone.com.eg,5000,Technology,02" > Employees.csv
+
+
 total_employees=$(cat Employees.csv | tail -n +2 | wc -l)
 echo "Total number of employees: $total_employees"
 
